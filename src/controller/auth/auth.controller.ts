4f38a5435ec8641ea78615b704f1345a8f291cfc
LuisLunaV <luis.a.luna.v@outlook.com>
2025-01-10 00:18:40 -0600

    Concluimos login

--- src/controller/auth/auth.controller.ts
+++ src/controller/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { User } from '../../model/auth/users';
 import { BcryptAdapter } from '../../config/bcrypt';
-
+import { JwtAdapter } from '../../config/jwt';
 
 
 export class AuthController{
@@ -21,8 +21,8 @@ export class AuthController{
             const user = await User.create( data );
 
             res.status(201).json({
-               'msg':'Usuario registrado',
                'status':201,
+               'message':'Usuario registrado',
                user
            });
 
@@ -33,21 +33,51 @@ export class AuthController{
         }
     }
 
-    static async login( req:Request, res:Response ):Promise<void>{
+    static async login( req:Request, res:Response ):Promise<any>{
       try {
         const { User_Email, User_Password } = req.body;
-        console.log(User_Email)
 
        const user = await User.findOne({ where:{ User_Email } });
-console.log(user)
+
+       if( !user ){
+        return res.status(401).json({
+            "status": 401,
+            "error": "Unauthorized",
+            "message": "El usuario no existe."
+        });
+       }
+
+       const comparePassword = BcryptAdapter.compare;
+       const isMatched = comparePassword( User_Password, user!.User_Password);
+
+       if( !isMatched ){
+           return res.status(401).json({
+            "status": 401,
+            "error": "Unauthorized",
+            "message": "El password es incorrecto."
+        });
+       }
+       
+       const token = await JwtAdapter.generarToken({
+        id:user.User_Id,
+        name: user.User_Name,
+        email: user.User_Email
+       });
+
+       res.cookie('x-token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 12 * 60 * 60 * 1000 
+       })
+
        res.status(200).json({
-           'msg':'Login_controller',
            user
        });
        
       } catch (error) {
         res.status(500).json({
-            'Erro':'Internal Server Error'
+              'Erro':`Internal server error. ${ error }`
         });
       }
     }
